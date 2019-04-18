be9bf8feb8969b6379d922be4b33d06bdb642c5a
Robert Kowalski <rok@kowalski.gd>
2019-04-18 23:15:00 +0200

    pipe: test callback

--- test/pipe.js
+++ test/pipe.js
@@ -24,3 +24,26 @@ tape('simple pipe', function (t) {
   r.push('world')
   r.push(null)
 })
+
+tape('pipe with callback', function (t) {
+  const buffered = []
+
+  const r = new Readable()
+  const w = new Writable({
+    write (data, cb) {
+      buffered.push(data)
+      cb(null)
+    }
+  })
+
+  r.pipe(w, function (err) {
+    t.pass('callback called')
+    t.same(err, null)
+    t.same(buffered, [ 'hello', 'world' ])
+    t.end()
+  })
+
+  r.push('hello')
+  r.push('world')
+  r.push(null)
+})
