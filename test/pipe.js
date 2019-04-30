f07915c7853bcb3bb247126f39a240e00c0da780
Mathias Buus <mathiasbuus@gmail.com>
2019-04-30 11:26:27 +0200

    add pipe tests from @robertkowalski

--- test/pipe.js
+++ test/pipe.js
@@ -19,3 +19,50 @@ tape('pipe with callback - error case', function (t) {
   r.push('world')
   r.push(null)
 })
+
+tape('simple pipe', function (t) {
+  const buffered = []
+
+  const r = new Readable()
+  const w = new Writable({
+    write (data, cb) {
+      buffered.push(data)
+      cb(null)
+    },
+
+    final () {
+      t.pass('final called')
+      t.same(buffered, [ 'hello', 'world' ])
+      t.end()
+    }
+  })
+
+  r.pipe(w)
+
+  r.push('hello')
+  r.push('world')
+  r.push(null)
+})
+
+tape('pipe with callback', function (t) {
+  const buffered = []
+
+  const r = new Readable()
+  const w = new Writable({
+    write (data, cb) {
+      buffered.push(data)
+      cb(null)
+    }
+  })
+
+  r.pipe(w, function (err) {
+    t.pass('callback called')
+    t.same(err, null)
+    t.same(buffered, [ 'hello', 'world' ])
+    t.end()
+  })
+
+  r.push('hello')
+  r.push('world')
+  r.push(null)
+})
